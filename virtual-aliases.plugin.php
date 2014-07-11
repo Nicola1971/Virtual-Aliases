9410fe4676d07b0f8aaa434607b0cbe7ac6a86a0
Nicola1971 <modxit@modx.it>
2014-07-11 02:05:42 +0200

    Updated to vers 0.0.3

--- virtual-aliases.plugin.php
+++ virtual-aliases.plugin.php
@@ -1,26 +1,25 @@
 //<?php
 // Virtual Aliases
-// version 0.0.2
+// version 0.0.3
 // Allows for an unlimited number of custom aliases per page.
 // By Brian Stanback @ www.stanback.net
- 
+
 // On Install: Check the the "OnPageNotFound" box in the System Events tab.
 // Plugin configuration: &aliasesTV=Aliases TV name;string;Aliases
- 
+
 // For overriding documents, create a new template variabe (TV) named
 // Aliases with the following option:
 //    Input Type: Textarea (Smaller)
-// 
-// Aliases should be added to the TV, one per line. 
+//
+// Aliases should be added to the TV, one per line.
 // Omit any leading or trailing slashes as well as the default suffix (usaully .html)
- 
+
 // Begin plugin code
 $e = &$modx->event;
- 
-if ($e->name == "OnPageNotFound") 
-{
+
+if ($e->name == "OnPageNotFound") {
    // Retrieve requested path + alias
-   $documentAlias = trim($_SERVER['REQUEST_URI'], '/');
+   $documentAlias = str_replace($modx->config['friendly_url_suffix'], '', $_REQUEST['q']);
 
    // Search TVs for potential alias matches
    $sql = "SELECT tvc.contentid as id, tvc.value as value FROM " . $modx->getFullTableName('site_tmplvars') . " tv ";
@@ -29,26 +28,32 @@ if ($e->name == "OnPageNotFound")
    $sql .= "LEFT JOIN " . $modx->getFullTableName('site_content') . " sc ON sc.id = tvc.contentid ";
    $sql .= "WHERE sc.published = 1 AND tvtpl.templateid = sc.template AND tv.name = '$aliasesTV' AND tvc.value LIKE '%" . $modx->db->escape($documentAlias) . "%'";
    $results = $modx->dbQuery($sql);
- 
+
    // Attempt to find an exact match
    $found = 0;
-   while ($found == 0 && $row = $modx->db->getRow($results))
-   {
+   while ($found == 0 && $row = $modx->db->getRow($results)) {
       $pageAliases = explode("\n", $row["value"]);
-      while ($found == 0 && $alias = each($pageAliases))
-      {
-         if (trim($alias[1]) == $documentAlias)  // Check for a match
+      while ($found == 0 && $alias = each($pageAliases)) {
+         if (trim($alias[1]) == $documentAlias) {
+            // Check for a match
             $found = $row["id"];
+         }
       }
    }
- 
-   if($found)  // Redirect to new document, if an alias was found
-   {
-      $pageUrl = $modx->makeUrl($found, '', '', "full");
-      $modx->sendRedirect($pageUrl, 0, "REDIRECT_HEADER", 'HTTP/1.1 301 Moved Permanently');
+
+   if ($found) {
+      // Redirect to new document, if an alias was found
+      if ($found == $modx->config['site_start']) {
+         $pageUrl = $modx->config['site_url'];
+      } else {
+         $pageUrl = $modx->makeUrl($found, '', '', "full");
+      }
+
+      // Send a permanent redirect
+      $modx->sendRedirect($pageUrl, 0, "REDIRECT_HEADER", "HTTP/1.1 301 Moved Permanently");
       exit(0);
    }
- 
+
    header("HTTP/1.0 404 Not Found");
    header("Status: 404 Not Found");
 }
